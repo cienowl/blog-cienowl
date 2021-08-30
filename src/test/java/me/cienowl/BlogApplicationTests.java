1e79678647e8470200b1e4eb6b77bd82766e99c1
Hojoon Lee <leehj.david@gmail.com>
2021-08-30 23:49:32 +0900

    First commit

---
+++ src/test/java/me/cienowl/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.cienowl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
